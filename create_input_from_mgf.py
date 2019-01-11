90e291eb9020acc07730370acd280a15309a9a24
Your Name <you@example.com>
2019-01-11 13:18:39 +0100

    in house reproc + ionbot data

--- create_input_from_mgf.py
+++ create_input_from_mgf.py
@@ -21,10 +21,29 @@ def preproc_MS2data2moFF ( f1,name ) :
 
 
 
+
+def merge_omega_moFFquant(f1,f2,name,args):
+    omega_df =pd.read_csv(f1,sep=",")
+    # Case where spec_is is nameFile.sca.scan.charge
+    # it could change !!! 
+    omega_df['scan']=  omega_df['spec_id'].str.split(".",expand=True)[1]
+    omega_df['scan'] = omega_df['scan'].astype(int)
+    ms2raw_moff= pd.read_csv(f2,sep="\t")
+    ## clean data from moFF side
+    if ( ms2raw_moff[ ms2raw_moff['log_int'] == -1].shape[0] /  ms2raw_moff.shape[0] )  >=  0.5:
+        print ( '{%s} detected missing intensity more the O.5 pec'.format(f1))
+
+    ms2raw_moff = ms2raw_moff[ ms2raw_moff['log_int'] != -1]
+    
+    test= pd.merge(omega_df,ms2raw_moff[['scan','rt','mz','intensity','rt_peak','lwhm','rwhm','5p_noise','10p_noise','SNR','log_L_R','log_int']],on='scan',how='left')
+    test.to_csv(os.path.join(args.output, 'result',os.path.basename(name +'_omega_quant.txt')) ,sep='\t',index=False )
+    return 0
+
+
 def merge_mgf_moFFquant( f1,f2,name,args):
     mgf_df =pd.read_csv(f1,sep="\t")
     if mgf_df.SCANS.any() == 'null':
-        print '\t  Input file is based ONLY of ms2 scan for ',f1
+        print ('\t  Input file is based ONLY of ms2 scan for {%r} '.format(f1))
         return 2
     #print  mgf_df.columns
     mgf_df.drop(['RTINSECONDS','PEPMASS'],axis=1,inplace=True)
@@ -44,8 +63,8 @@ def merge_mgf_MS2data( f1,f2,name,args):
     #print mgf_df.shape, ms2raw_df.shape
     # convert to int the scan number
     if mgf_df.SCANS.any() == 'null':
-        print '\t  ','SCAN Id not available in the mgf file for ', f1
-        print '\t  Input file is based ONLY of ms2 scan'
+        print ('\t  ','SCAN Id not available in the mgf file for {%r} '.format(f1))
+        print ('\t  Input file is based ONLY of ms2 scan')
         ms2raw_df['CHARGE']=np.nan
         ms2raw_df['INDEX']=np.nan
         ms2raw_df = ms2raw_df[['CHARGE','INDEX','time','precursorMass','SCANS']]
@@ -100,7 +119,7 @@ def merge_moff_mztab(f1,f2,name ,args):
     #print 'missing hit',test[test['intensity']==-1]
     if test.shape [0] == mztab_df.shape[0]:
         test = test[test['intensity'] !=-1]
-        print '\t\t','final output size (filtered moff output) :',name ,test.shape 
+        print ('\t\t {%r}final output size (filtered moff output){%r}:'.format(name ,test.shape) )
         
         test.to_csv(os.path.join(args.output, os.path.basename(name +'.ms1_quant')) ,sep='\t',index=False )
         return 0
@@ -164,7 +183,7 @@ def check_scannumber(file2scan,args):
     f= open(os.path.join(os.getcwd(),file2scan),'r')
     f2=open ( os.path.join(args.output,output_name_mgfparsed),'w' )
     f2.write('\t'.join(col_list)+'\n' )
-    print '# Spectra',len(pos)
+    print ('# Spectra {%i}'.format(len(pos)))
     total=f.readlines()
     # for just one spectra !i
     #mass =-1
@@ -197,15 +216,15 @@ def check_scannumber(file2scan,args):
 def run_parser (args):
     rt_tag=[]
     scan_tag=[]
-    print args.start + '/submitted/*.mgf'
+    print( args.start + '/submitted/*.mgf')
     for name in glob.glob(args.start + '/submitted/*.mgf'):
-        print '\t', name
+        print ('\t' + name)
         #print file_in
         res = check_scannumber(name,args)
         # res containes the path and file name for the two file of the marging and just the name of file
         final_data = merge_mgf_MS2data( res[0],res[1],res[2],args)
         if final_data == -1:
-            print 'Error : Joinning failed No input file for ',name
+            print ('Error : Joinning failed No input file for  {%s}'.format(name))
         
 
     return 0
@@ -215,25 +234,43 @@ def run_parser (args):
 
 
 def run_preproc (args):
-    print args.start + '/*.ms2scan'
+    print (args.start + '/*.ms2scan')
             #mgf_proc= sorted(glob.glob(args.start + '/*.moff2start' ))
     scan_proc= sorted(glob.glob(args.start + '/*.ms2scan' ))
             #print len (mgf_proc),len(scan_proc)
             #print mgf_proc[0]
             #print  scan_proc[0]
     for ii in range(0,len(scan_proc)):
-        print '\t', scan_proc[ii]
+        print ('\t' + scan_proc[ii])
         final_data =  preproc_MS2data2moFF ( scan_proc[ii],os.path.basename(scan_proc[ii]).split('.')[0] )
         if final_data == -1:
-            print 'Error : preproc failed  for ' , scan_proc[ii]
+            print ('Error : preproc failed  for {%s} '.format(scan_proc[ii]))
             exit(1 )
     return 1
-                                                                                                                                                                                                         
+        
+def run_join_omega (args):
+    print (args.start + '/omega/*.mgf.omega.csv')
+
+    omega_proc= sorted(glob.glob(args.start + '/omega/*.mgf.omega.csv' ))
+    scan_proc= sorted(glob.glob(args.output + '/moff_output/*_moff_result.txt' ))
+    print ( len(omega_proc), len(scan_proc) )
+    for ii in range(len(omega_proc)) :
+        print ('\t' +  omega_proc[ii] + ' -- ' + scan_proc[ii] )
+        final_data = merge_omega_moFFquant( omega_proc[ii],scan_proc[ii],os.path.basename(omega_proc[ii]).split('.')[0] ,args )
+        if final_data == -1:
+            print ('Error : Join failed No input file for {%s} '.format(omega_proc[ii]) )
+            exit(1 )
+
+    if final_data == 2:
+        return 1
+    else:
+        return 0
+
 
 # run methods for the mgf  to MS2 data 
 #Still used in the pipeline
 def run_join (args):
-        print args.start + '/*.moff2start'
+        print (args.start + '/*.moff2start')
         
         mgf_proc= sorted(glob.glob(args.start + '/*.moff2start' ))
         
@@ -244,18 +281,18 @@ def run_join (args):
         scan_proc= sorted(glob.glob(args.start + '/moff_output/*_moff_result.txt' ))
         #------
 
-        print len (mgf_proc),len(scan_proc)
+        print (len (mgf_proc) , len(scan_proc))
         #print mgf_proc[0]
         #print  scan_proc[0]
         for ii in range(0,len(mgf_proc)):
-            print '\t', mgf_proc[ii],scan_proc[ii]
+            print ('\t' +  mgf_proc[ii] + '_' +  scan_proc[ii] )
             
             # res containes the path and file name for the two file of the marging and just the name of file
             final_data = merge_mgf_moFFquant( mgf_proc[ii],scan_proc[ii],os.path.basename(mgf_proc[ii]).split('.')[0] ,args )
             # for prex version of the pipeline
             #final_data = merge_mgf_MS2data(mgf_proc[ii],scan_proc[ii],os.path.basename(mgf_proc[ii]).split('.')[0] ,args)
             if final_data == -1:
-                print 'Error : Join failed No input file for ' , mgf_proc[ii] 
+                print ('Error : Join failed No input file for {%s} '.format(mgf_proc[ii]) )
                 exit(1 )
 
         if final_data==2:
@@ -266,7 +303,7 @@ def run_join (args):
 # parsing moff result with the pride.txt result (from mztab)
 # not used anymore , this step will be embedded into tha java code
 def run_join_mztab( args):
-    print args.start + '/*_moff_result_ms2id.txt'
+    print (args.start + '/*_moff_result_ms2id.txt')
     # debug for pipeline drived by ms2 from mgf
     #moff_proc= sorted(glob.glob(args.start + '/*_moff_result.txt' ))
     # for new version of the pieeline
@@ -283,16 +320,16 @@ def run_join_mztab( args):
         #print '\t', moff_proc[ii]
         res =  [ s for s in mztab_proc  if os.path.basename(moff_proc[ii]).split('_moff_result_ms2id.txt')[0]  in s]
         if len(res) >= 1:
-            print 'merging between ',  moff_proc[ii], res[0]
+            print ('merging between {%r} {%r} '.format(  moff_proc[ii], res[0]))
             final_data = merge_moff_mztab(moff_proc[ii],res[0],os.path.basename(res[0]).split('.')[0] ,args)
             if final_data == -1:
-                print 'Error : Merging failed No input file for ' ,  moff_proc[ii],res[0]  
+                print ('Error : Merging failed No input file for {%r} {%r}' ,  moff_proc[ii],res[0] )
                 exit(1)
             # res containes the path and file name for the two file of the marging and just the name of 
     return 0
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Checking rt or scan number throw the Pride dataset ')
+    parser = argparse.ArgumentParser(description='General Purpose code for join and preprocessin the moFF pipeline for Pride data preprocessing ')
 
     parser.add_argument('--start_folder', dest='start', action='store',
                         help='specify the folder of the input MS2 peptide files  REQUIRED]', required=True)
@@ -309,6 +346,9 @@ if __name__ == '__main__':
 
     if args.type=='mgf':
         result_pride_id = run_join(args)
+
+    if args.type=='omega':
+        result_pride_id = run_join_omega(args)
     # actually not used but implemented using a java
     if args.type=='mztab':
         result_pride_id = run_join_mztab(args)
